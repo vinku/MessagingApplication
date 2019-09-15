518d8a0683872f361c31789f2c3672fc8af2e829
Vinay Kumar Sharma <vinakum@microsoft.com>
2019-09-15 13:30:42 +0530

    Adding files missed in last commit

---
+++ src/MessagingService.Data/Repository.cs
@@ -0,0 +1,50 @@
+﻿using MessagingService.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MessagingService.Data
+{
+	public class Repository
+	{
+		private MessaginContext _context;
+
+		public Repository(MessaginContext context)
+		{
+			_context = context;
+			_context.ChangeTracker.QueryTrackingBehavior = Microsoft.EntityFrameworkCore.QueryTrackingBehavior.NoTracking;
+		}
+
+		public User GetUserFromCellNumber(string cellNumber)
+		{
+			var user =
+				_context.Users
+				.FirstOrDefault(s => s.UserCellId == cellNumber);
+			return user;
+		}
+
+		public User GetUserFromCellNumberWithChats(string cellNumber)
+		{
+			var user =
+				_context.Users
+				.Include(u => u.UserChats)
+				.FirstOrDefault(u => u.UserCellId == cellNumber);
+			return user;
+		}
+
+		public void AddNewuser(User user)
+		{
+			// ToDo : Remove this method and add a save method which would work for both update and creation of new resource.
+			_context.Users.Add(user);
+			_context.SaveChanges();
+		}
+
+		public bool UseridExists(string cellNumber)
+		{
+			return GetUserFromCellNumber(cellNumber) != null;
+		}
+	}
+}
---
+++ src/MessagingService.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MessagingService.Data;
+using MessagingService.Domain;
+using Microsoft.AspNetCore.Rewrite.Internal.UrlActions;
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+
+namespace MessagingWebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		Repository _repo;
+
+		public UsersController(Repository repo)
+		{
+			_repo = repo;
+		}
+
+		// GET api/users
+		[HttpGet]
+		public ActionResult Get()
+		{
+			return NotFound();
+		}
+
+		// GET api/users/5
+		[HttpGet("{id}", Name = "GetIndividualUser")]
+		public ActionResult Get(string id)
+		{
+			return Ok(_repo.GetUserFromCellNumber(id));
+		}
+
+		// POST api/users
+		[HttpPost]
+		public ActionResult Post([FromBody] User user)
+		{
+			if (_repo.UseridExists(user.UserCellId))
+			{
+				return Conflict("User with same cell number already exists, can't add it any more");
+			}
+
+			// NOTE: there has to be a way to validate a phone number (OTP verification). For our case, we assue the cell number has to be a 10 digit string.
+			if (user.UserCellId.Length != 10)
+			{
+				return BadRequest("Phone number must be a 10-digit number");
+			}
+
+			// the creation time is the last seen time for now.
+			user.LastSeenTime = DateTime.Now;
+			_repo.AddNewuser(user);
+			return CreatedAtRoute("GetIndividualUser", new
+				{ id = user.UserCellId }, user);
+		}
+
+		// PUT api/users/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+			throw new NotImplementedException();
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
